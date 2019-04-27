a618c53989b289d5e8b003568a4c1ee74ec37bf5
Alexis Destrez <alexis.destrez@schibsted.com>
2019-04-27 14:08:40 +0200

    Don't repeat file name in wrap as open alreadu do it

--- Makefile
+++ Makefile
@@ -3,7 +3,6 @@
 .PHONY: help
 help:
 	@echo "Available targets descriptions:"
-	@# absolutely awesome -> http://marmelab.com/blog/2016/02/29/auto-documented-makefile.html
 	@grep -E '^[%a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
 
 .PHONY: lint-all lint-go lint-markdown lint-yaml
--- source/file/file.go
+++ source/file/file.go
@@ -57,9 +57,7 @@ func (f *File) Name() string {
 func (f *File) Unmarshal(to interface{}) error {
 	ff, err := f.fs.Open(f.path)
 	if err != nil {
-		return trivialerr.WrapIf(f.strictOpen,
-			errors.Wrapf(err, "unable to open file %q", f.path),
-		)
+		return trivialerr.WrapIf(f.strictOpen, errors.Wrap(err, "unable to open file"))
 	}
 	defer ff.Close() // nolint: errcheck, gosec
 
